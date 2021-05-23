152b2878f6dccf1b9c985019ab6b724cf0a077b3
minternational <mintkuehn@gmail.com>
2021-05-23 20:29:33 +0200

    add counter for the final piece release and add the new youtube teaser trailer 2 link

--- src/components/art/Art.jsx
+++ src/components/art/Art.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import Countdown from "../countdown/Countdown";
 import Content from "./Content";
 import Video from "./Video";
-import Dollarbill from "./Dollarbill";
 import "../../styles/general-styles.css";
 import "./art-styles.css";
 
 const Art = () => {
   return (
     <section id="art">
+      <Countdown
+        timeTillDate="04 06 2021, 7:30 pm"
+        timeFormat="DD MM YYYY, h:mm a"
+      />
       <Content />
       <Video />
-      <Dollarbill />
     </section>
   );
 };
--- src/components/art/Video.jsx
+++ src/components/art/Video.jsx
@@ -6,7 +6,7 @@ const Video = () => {
       <iframe
         width="100%"
         height="720px"
-        src="https://www.youtube.com/embed/8MT1bpeOEjc"
+        src="https://www.youtube.com/embed/pqVIyEMzbPI"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
